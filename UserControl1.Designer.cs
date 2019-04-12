2a589aff0fd1c6323cf79e680c150b6a82cc0aff
MrMarkMichael <47720374+MrMarkMichael@users.noreply.github.com>
2019-04-12 09:02:35 +0200

    Imported

---
+++ UserControl1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace IntPayCalc
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskedTextBox4 = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskedTextBox5 = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 80);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(189, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Contract Date";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(248, 74);
+            this.maskedTextBox1.Mask = "00/00/0000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(526, 38);
+            this.maskedTextBox1.TabIndex = 1;
+            this.maskedTextBox1.ValidatingType = typeof(System.DateTime);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(248, 118);
+            this.maskedTextBox2.Mask = "00/00/0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(526, 38);
+            this.maskedTextBox2.TabIndex = 3;
+            this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 124);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label2.Size = new System.Drawing.Size(225, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Calculation Date";
+            // 
+            // maskedTextBox3
+            // 
+            this.maskedTextBox3.Location = new System.Drawing.Point(248, 206);
+            this.maskedTextBox3.Mask = "00000.000";
+            this.maskedTextBox3.Name = "maskedTextBox3";
+            this.maskedTextBox3.Size = new System.Drawing.Size(526, 38);
+            this.maskedTextBox3.TabIndex = 7;
+            this.maskedTextBox3.ValidatingType = typeof(int);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 212);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(176, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Interest Rate";
+            // 
+            // maskedTextBox4
+            // 
+            this.maskedTextBox4.Location = new System.Drawing.Point(248, 162);
+            this.maskedTextBox4.Mask = "00000000000000000.00";
+            this.maskedTextBox4.Name = "maskedTextBox4";
+            this.maskedTextBox4.Size = new System.Drawing.Size(526, 38);
+            this.maskedTextBox4.TabIndex = 5;
+            this.maskedTextBox4.ValidatingType = typeof(int);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 168);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(203, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Value Invested";
+            // 
+            // maskedTextBox5
+            // 
+            this.maskedTextBox5.Location = new System.Drawing.Point(248, 250);
+            this.maskedTextBox5.Mask = "00000";
+            this.maskedTextBox5.Name = "maskedTextBox5";
+            this.maskedTextBox5.Size = new System.Drawing.Size(526, 38);
+            this.maskedTextBox5.TabIndex = 9;
+            this.maskedTextBox5.ValidatingType = typeof(int);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 256);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(193, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Anual Periods";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 331);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 82);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 375);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(526, 38);
+            this.textBox1.TabIndex = 11;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(40, 40);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(242, 331);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(260, 32);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Sum of Interest Left";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label7.Location = new System.Drawing.Point(9, 4);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 32);
+            this.label7.TabIndex = 14;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.maskedTextBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskedTextBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ UserControl1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntPayCalc
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool mCheck = true;
+            if (!checkIfDate(maskedTextBox1.Text) & mCheck) { maskedTextBox1.Text = ""; mCheck = false; }
+
+            if (!checkIfDate(maskedTextBox2.Text) & mCheck) { maskedTextBox2.Text = ""; mCheck = false; }
+
+            float valf;
+            float intf;
+            int napi;
+
+            if (!float.TryParse(maskedTextBox4.Text, out valf) & mCheck) { maskedTextBox4.Text = ""; mCheck = false; }
+
+            if (!float.TryParse(maskedTextBox3.Text, out intf) & mCheck) { maskedTextBox3.Text = ""; mCheck = false; }
+
+            if (!int.TryParse(maskedTextBox5.Text, out napi) & mCheck) { maskedTextBox5.Text = ""; mCheck = false; }
+            float retF;
+            string returnstring;
+
+            if (mCheck) {
+                label7.Text = "";
+                retF = ipCalculate(maskedTextBox1.Text, maskedTextBox2.Text, valf, intf, napi);
+                returnstring = Convert.ToString(retF);
+                textBox1.Text = returnstring;
+            }
+            else{
+                label7.Text = "Error: Ammend the input values!";
+            }
+        }
+
+
+        private bool checkIfDate(string intext)
+        {
+            bool strChecked;
+            
+            DateTime dDate;
+
+            if (DateTime.TryParse(intext, out dDate))
+            {
+                strChecked = true;
+            }
+            else
+            {
+                strChecked = false;
+            }
+            return strChecked;
+        }
+
+        private float ipCalculate (string inDate, string calcDate, float val, float cup, int napf)
+        {
+            float intAm = 0;
+            float decr1;
+            int sumOfPaidm;
+            int numPerLeft;
+
+            DateTime conDate;
+            DateTime calDate;
+            DateTime.TryParse(inDate, out conDate);
+            DateTime.TryParse(calcDate, out calDate);
+            sumOfPaidm = ((calDate.Year - conDate.Year) * 12) + calDate.Month - conDate.Month;
+            if (calDate.Day - conDate.Day < 0 ) { sumOfPaidm--; }
+
+            numPerLeft = (napf * 12 - sumOfPaidm);
+            val = val - val*numPerLeft/(napf*12);
+            for (int i = 0; i < numPerLeft; i++)
+            {
+               
+                intAm = intAm + cup*val / 100;
+                val = val - val / (napf * 12);
+     
+            }
+            
+
+            return intAm;
+        }
+
+    }
+}
